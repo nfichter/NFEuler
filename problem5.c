98dcef500c3c39fe5bfa1f1ae0ad151fdc39cf19
nfichter <nfichter@stuy.edu>
2016-09-18 20:42:42 -0400

    Reworked algorithm to a much faster brute force algorithm

--- problem5.c
+++ problem5.c
@@ -4,24 +4,27 @@
 #include <stdlib.h>
 
 int main() {
-  int counter = 1;
-  int oneToTwenty = 1;
-  int numCorrect = 0;
-  while (1) {
+  int num = 20; //outcome - start this at 20 since we know it will have to be divisile by 20 (increment by 20 every time)
+  int oneToTwenty = 1; //counter for 1-20
+  int numCorrect = 0; //if this == 20 at the end of a loop, we know it's correct
+  while (numCorrect != 20) {
     numCorrect = 0;
-    for (oneToTwenty = 1; oneToTwenty < 21; oneToTwenty++) {
-      if (counter % oneToTwenty != 0) {
+    /*
+    for (oneToTwenty = 1; oneToTwenty < 21; oneToTwenty++) { *THIS VERSION IS VERY SLOW, WHICH IS WHY I CHANGED TO BRUTE FORCE ALGORITHM*
+      if (num % oneToTwenty != 0) {
 	break;
       }
       else {
 	numCorrect++;
       }
     }
-    if (numCorrect == 20) {
-      break;
+    */
+    //brute force: skip 1 (all #s are mult), 6 (if mult of 2 & 3 then mult of 6), 10 (2&5 --> 10), 12 (3&4 --> 12), 14 (2&7 --> 14), 15 (3&5 --> 15), 18 (2&9 --> 18)
+    if (num % 2 == 0 && num % 3 == 0 && num % 4 == 0 && num % 5 == 0 && num % 7 == 0 && num % 8 == 0 && num % 9 == 0 &&
+	num % 11 == 0 && num % 13 == 0 && num % 16 == 0 && num % 17 == 0 && num % 19 == 0 && num % 20 == 0) {
+      numCorrect = 20;
     }
-    printf("%d %d\n",counter, numCorrect);
-    counter++;
+    num += 20;
   }
-  printf("Answer to problem 5:  %d\n",counter);
+  printf("Answer to problem 5:  %d\n",num);
 }
